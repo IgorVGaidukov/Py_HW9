215e60fc1ff5772ddd47023aed9b66fa02981f81
Admin <msk_moz_info@kinomax.ru>
2023-02-09 21:44:23 +0300

    Add solutions

--- Task1.py
+++ Task1.py
@@ -28,3 +28,40 @@
 8 10 12
 14 16 18
 """
+
+
+class Matrix:
+
+    def __init__(self, my_list):
+        self.my_list = my_list
+
+    def __str__(self):
+        for el in self.my_list:
+            print(*el)
+        return ''
+
+    def __add__(self, other_matrix):
+        out_matrix = []
+        for i in range(len(self.my_list)):
+            tmp_list = []
+            for j in range(len(self.my_list[i])):
+                tmp_list.append(self.my_list[i][j] + other_matrix.my_list[i][j])
+            out_matrix.append(tmp_list)
+        return Matrix(out_matrix)
+
+
+lst1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+lst2 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+matrix1 = Matrix(lst1)
+matrix2 = Matrix(lst2)
+matrix3 = matrix1 + matrix2
+
+print('Исходная матрица 1')
+print(matrix1)
+
+print('Исходная матрица 2')
+print(matrix2)
+
+print('Сумма матриц')
+print(matrix3)
--- Task2.py
+++ Task2.py
@@ -5,3 +5,22 @@
 Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+
+
+class MyZeroDivideException(Exception):
+
+    def __init__(self, msg):
+        self.msg = msg
+
+
+dig1 = int(input('Введите число 1: '))
+dig2 = int(input('Введите число 2: '))
+
+try:
+    if dig2 == 0:
+        raise MyZeroDivideException('Деление на 0!')
+    res = dig1 / dig2
+except MyZeroDivideException as err:
+    print(err)
+else:
+    print(f'Результат: {res}')
